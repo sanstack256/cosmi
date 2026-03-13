279d8e0e4fe99de1428d76daf82c6c14c1a4e527
N S Sanjeev <nssanjeev@Sans-MacBook-Air.local>
2026-03-13 22:57:20 +0530

    Lordicon bug fix 2

--- types/lord-icon.d.ts
+++ types/lord-icon.d.ts
@@ -1,20 +1,21 @@
 // types/lord-icon.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    "lord-icon": any;
+
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "lord-icon": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        trigger?: string;
+        colors?: string;
+        stroke?: string;
+      };
+    }
   }
 }
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "lord-icon": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      src?: string;
-      trigger?: string;
-      colors?: string;
-      stroke?: string;
-    };
-  }
-}
+export {};
